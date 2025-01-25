f12947afceeb2b390545f74cac8becefb04f37b3
Kostandin Grabovaj <kostandin.grabovaj@gmail.com>
2025-01-25 02:56:03 +0100

    Pokedex: Added swagger-ui

--- src/main/java/com/example/pokedex/web/PokemonController.java
+++ src/main/java/com/example/pokedex/web/PokemonController.java
@@ -1,5 +1,12 @@
 package com.example.pokedex.web;
 
+import com.example.pokedex.web.dto.ErrorMessage;
+import com.example.pokedex.web.dto.Pokemon;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +25,51 @@ public class PokemonController {
         this.handler = handler;
     }
 
+    /**
+     * Get Pokemon information
+     * @param name pokemon name
+     * @return Pokemon basic information
+     */
+    @Operation(
+        summary = "Get pokemon information",
+        description = "By passing a name as path variable it will download basic pokemon information"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retried pokemon",
+            content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = Pokemon.class)) }
+        ),
+        @ApiResponse(responseCode = "404", description = "Pokemon not found",
+            content = {
+                @Content(schema = @Schema(implementation = ErrorMessage.class))
+            })
+    })
     @GetMapping("{name}")
-    public ResponseEntity<?> getPokemon(@PathVariable("name") String name) {
+    public ResponseEntity<Pokemon> getPokemon(@PathVariable("name") String name) {
         return ResponseEntity.ok(handler.getPokemon(name));
     }
 
+    /**
+     * Get Pokemon Translations
+     * @param name pokemon name
+     * @return Pokemon information with translated description
+     */
+    @Operation(
+        summary = "Get pokemon fun translation",
+        description = "By passing a name as path variable it will download basic pokemon information with translated description"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retried pokemon",
+            content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = Pokemon.class)) }
+        ),
+        @ApiResponse(responseCode = "404", description = "Pokemon not found",
+            content = {
+                @Content(schema = @Schema(implementation = ErrorMessage.class))
+            })
+    })
     @GetMapping("translated/{name}")
-    public ResponseEntity<?> getPokemonTranslation(@PathVariable("name") String name) {
+    public ResponseEntity<Pokemon> getPokemonTranslation(@PathVariable("name") String name) {
         return ResponseEntity.ok(handler.getPokemonTranslation(name));
     }
 }
